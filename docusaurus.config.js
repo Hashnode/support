f3966083ed031d4a7dfc1432a1ca0fc5f1b1690f
Megha <meghapathak2013@gmail.com>
2021-01-18 13:47:19 +0530

    upload from local

---
+++ docusaurus.config.js
@@ -0,0 +1,105 @@
+module.exports = {
+  title: "Welcome to Hashnode Guide",
+  tagline: "Your Go-to resource to understand Hashnode a little better.",
+  url: "https://your-docusaurus-test-site.com",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+  organizationName: "facebook", // Usually your GitHub org/user name.
+  projectName: "docusaurus", // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: "Hashnode Guide",
+      logo: {
+        alt: "My Site Logo",
+        src: "img/logo.svg",
+      },
+      items: [
+        {
+          to: "docs/",
+          activeBasePath: "docs",
+          label: "Guides",
+          position: "left",
+        },
+        { to: "blog", label: "Townhall", position: "left" },
+        {
+          href: "https://github.com/facebook/docusaurus",
+          label: "GitHub",
+          position: "right",
+        },
+      ],
+    },
+    footer: {
+      style: "dark",
+      links: [
+        {
+          title: "Guides",
+          items: [
+            {
+              label: "Style Guide",
+              to: "guides/",
+            },
+            {
+              label: "Second Doc",
+              to: "docs/doc2/",
+            },
+          ],
+        },
+        {
+          title: "Community",
+          items: [
+            {
+              label: "Stack Overflow",
+              href: "https://stackoverflow.com/questions/tagged/docusaurus",
+            },
+            {
+              label: "Discord",
+              href: "https://discordapp.com/invite/docusaurus",
+            },
+            {
+              label: "Twitter",
+              href: "https://twitter.com/docusaurus",
+            },
+          ],
+        },
+        {
+          title: "More",
+          items: [
+            {
+              label: "Blog",
+              to: "blog",
+            },
+            {
+              label: "GitHub",
+              href: "https://github.com/facebook/docusaurus",
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Hashnode Guide`,
+    },
+  },
+  presets: [
+    [
+      "@docusaurus/preset-classic",
+      {
+        docs: {
+          sidebarPath: require.resolve("./sidebars.js"),
+          // Please change this to your repo.
+          editUrl:
+            "https://github.com/facebook/docusaurus/edit/master/website/",
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            "https://github.com/facebook/docusaurus/edit/master/website/blog/",
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      },
+    ],
+  ],
+};
