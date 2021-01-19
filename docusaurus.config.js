a51a39c356b2abf7f7b7de22f680c0c4c8a5e879
Megha Pathak <meghapathak2013@gmail.com>
2021-01-19 12:47:30 +0530

    updated footer

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -77,7 +77,7 @@ module.exports = {
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} Hashnode`,
     },
   },
   presets: [
