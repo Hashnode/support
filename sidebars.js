cdd0d2d203b27eb73252ba58b6e6f5fb1e811094
Megha Pathak <meghapathak2013@gmail.com>
2021-01-20 17:57:50 +0530

    Added Categories to the Sidebar

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,10 @@
 module.exports = {
   someSidebar: {
-    Docusaurus: ['doc1', 'doc2', 'doc3'],
-    Features: ['mdx'],
+    "Getting Started": ['using-hashnode', 'hashnode-glossary', 'create-team-blog', 'create-personal-blog', 'contact-support','change-log','bug-reports-and-feature-request'],
+    "Mapping to Custom Domain": ['mapping-domain','mapping-cloudfare', 'mapping-godaddy', 'mapping-namecheap'],
+    "Writing and Editing" :['write-an-article', 'tags', 'markdown-guidelines', 'embeds', 'edit-url-type', 'edit-slug', 'edit-article', 'edit-drafts', 'cover-photo'], 
+    "Settings & Customization":['account-settings', 'analytics', 'export-articles','general-settings', 'github-backup', 'import-articles','integrations', 'newsletter', 'seo', 'static-pages',  'teams-blog-settings', 'widgets' ],
+    "Safety & Polices": ['community-code-of-conduct', 'report-copyright-infringement', 'report-posts-and-users'], 
+    "FAQs" : ['faqs'],
   },
 };
