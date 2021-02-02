a0500a1bdb3e34b0bf1b61a8d65506a77dd434be
Megha Pathak <meghapathak2013@gmail.com>
2021-02-02 19:35:22 +0530

    Revert Analytics

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,33 +1,36 @@
 module.exports = {
-  title: 'Hashnode Support',
-  tagline: 'Your go-to guides for understanding Hashnode a little better',
-  url: 'https://support.hashnode.com/',
-  baseUrl: '/',
-  onBrokenLinks: 'ignore',
-  onBrokenMarkdownLinks: 'ignore',
-  favicon: 'https://cdn.hashnode.com/res/hashnode/image/upload/v1611242173172/AOX1gE2jc.png',
-  organizationName: 'hashnode', // Usually your GitHub org/user name.
-  projectName: 'support', // Usually your repo name.
+  title: "Hashnode Support",
+  tagline: "Your go-to guides for understanding Hashnode a little better",
+  url: "https://support.hashnode.com/",
+  baseUrl: "/",
+  onBrokenLinks: "ignore",
+  onBrokenMarkdownLinks: "ignore",
+  favicon:
+    "https://cdn.hashnode.com/res/hashnode/image/upload/v1611242173172/AOX1gE2jc.png",
+  organizationName: "hashnode", // Usually your GitHub org/user name.
+  projectName: "support", // Usually your repo name.
   themeConfig: {
     navbar: {
       // title: 'Hashnode',
       logo: {
-        alt: 'Hashnode Support Logo',
-        src: 'https://cdn.hashnode.com/res/hashnode/image/upload/v1611856421169/zcx6qvf_J.png?auto=compress',
-        srcDark: 'https://cdn.hashnode.com/res/hashnode/image/upload/v1611856682886/N1xqQtDit.png?auto=compress'
+        alt: "Hashnode Support Logo",
+        src:
+          "https://cdn.hashnode.com/res/hashnode/image/upload/v1611856421169/zcx6qvf_J.png?auto=compress",
+        srcDark:
+          "https://cdn.hashnode.com/res/hashnode/image/upload/v1611856682886/N1xqQtDit.png?auto=compress",
       },
       items: [
         {
-          href: 'https://hashnode.com',
-          label: 'Back to Hashnode',
-          position: 'right',
+          href: "https://hashnode.com",
+          label: "Back to Hashnode",
+          position: "right",
         },
       ],
     },
     algolia: {
-      apiKey: 'b49750d742345b19c79e80a2e3e20312',
-      indexName: 'docs',
-      appId : 'AMERDMZM12',
+      apiKey: "b49750d742345b19c79e80a2e3e20312",
+      indexName: "docs",
+      appId: "AMERDMZM12",
       // Optional: see doc section bellow
       contextualSearch: true,
 
@@ -38,55 +41,41 @@ module.exports = {
     },
     footer: {
       links: [
-        
         {
-          title: 'Talk to the team',
+          title: "Talk to the team",
           items: [
             {
-              label: 'Email Us',
-              href: 'mailto:hello@hashnode.com',
+              label: "Email Us",
+              href: "mailto:hello@hashnode.com",
             },
             {
-              label: 'Twitter',
-              href: 'https://twitter.com/hashnode',
+              label: "Twitter",
+              href: "https://twitter.com/hashnode",
             },
             {
-              label: 'Discord',
-              href: 'https://discord.gg/qsAQfxX',
+              label: "Discord",
+              href: "https://discord.gg/qsAQfxX",
             },
           ],
         },
-        
       ],
       copyright: `Copyright © ${new Date().getFullYear()} Hashnode`,
     },
-   
-  
-  
   },
   presets: [
     [
-      '@docusaurus/preset-classic',
+      "@docusaurus/preset-classic",
       {
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
+          sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
-          editUrl:
-            'https://github.com/Hashnode/support/blob/main/',
+          editUrl: "https://github.com/Hashnode/support/blob/main/",
         },
-    
+
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       },
     ],
   ],
-  plugins: ['@docusaurus/plugin-google-analytics'],
-  themeConfig: {
-    googleAnalytics: {
-      trackingID: 'G-BGRTB830NS',
-      // Optional fields.
-      anonymizeIP: true, // Should IPs be anonymized?
-    },
-  },
 };
