025ea071d6a8f6c0326805040dff8b05ae191da5
Edidiong Asikpo <edidiongasikpo@gmail.com>
2021-06-15 10:06:30 +0100

    Add Hashnode Sponsors

--- src/pages/index.js
+++ src/pages/index.js
@@ -46,6 +46,7 @@ const features = [
     description: (
       <> 
         <li><a href = "docs/ambassador-program">Hashnode Ambassador</a></li>
+        <li><a href = "docs/hashnode-sponsors">Hashnode Sponsors</a></li>
       </>
     ),
   },
